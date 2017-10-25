b4852117ce73cb3ae9cda86461c5b207f930eaef
Davy'd <pdaswift3009@gmail.com>
2017-10-25 19:22:49 +0100

    mlab config

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var express = require('express'),
 var app = express();
 
 
-const db = mongoose.connect("mongodb://localhost:27017/Farm_passportAuth" || "mongodb://davycode:swood/66@ds231715.mlab.com:31715/agrotech" ,(err) => {
+const db = mongoose.connect("mongodb://davycode:swood/66@ds231715.mlab.com:31715/agrotech" ,(err) => {
     (err) ? console.error(err, 'Error Connecting to Database!'): console.log('DB Connected. Build Safely!');
 });
 
